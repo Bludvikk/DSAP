e78c60ac6a217e371e4c230d3924ed06e6583d7d
Bludvikk <cleverdmasoy@gmail.com>
2023-08-03 12:25:47 +0800

    yawa

--- app/Components/modal/EventModal.tsx
+++ app/Components/modal/EventModal.tsx
@@ -18,8 +18,6 @@ import useWriteNewsModal from "@/app/hooks/useWriteEventModal";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { DatePickerDemo } from "../Input/DatePicker";
-import Image from "next/image";
-import { revalidateTag } from "next/cache";
 import { useRouter } from "next/navigation";
 
 interface WriteEventModalProps {
