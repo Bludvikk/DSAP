f96fe14b0be14f246d70e7c26b88a9d222233dc4
bnguyen <bnguyen@surepoint.com>
2024-04-18 22:27:15 -0500

    added array

--- app/Components/page-content/LatestNews.tsx
+++ app/Components/page-content/LatestNews.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import Card from "../Card";
-import { motion } from "framer-motion";
-import { easeIn } from "framer-motion";
-import { easeOut } from "framer-motion";
-import moment from "moment";
-import parse from "html-react-parser";
+import React, { useEffect, useState } from 'react';
+import Card from '../Card';
+import { motion } from 'framer-motion';
+import { easeIn } from 'framer-motion';
+import { easeOut } from 'framer-motion';
+import moment from 'moment';
+import parse from 'html-react-parser';
 interface CardData {
   id: number;
   date: string;
@@ -16,7 +16,9 @@ interface CardData {
 }
 
 const LatestNews = () => {
-  const [selectedCard, setSelectedCard] = useState<number | null>(null);
+  const [selectedCard, setSelectedCard] = useState<number | null>(
+    null
+  );
   const [cardData, setCardData] = useState<CardData[]>([]);
   const handleCardHover = (cardId: number) => {
     setSelectedCard(cardId);
@@ -26,7 +28,7 @@ const LatestNews = () => {
     fetch(`api/news`, { next: { revalidate: 10 } })
       .then((response) => {
         if (!response.ok) {
-          throw Error("Error");
+          throw Error('Error');
         }
         return response.json();
       })
@@ -46,25 +48,28 @@ const LatestNews = () => {
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           whileInView={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5, type: "spring" }}
+          transition={{ duration: 0.5, type: 'spring' }}
           className="flex flex-row gap-4 md:gap-6 lg:gap-12 xl:gap-16"
         >
-          {cardData.map((card) => {
-            const initialSentences =
-              card.content.split(".").slice(0, 1).join(". ") + ".";
-            const formattedDate = moment(card.date).format("MMM - Do");
-            return (
-              <Card
-                key={card.id}
-                date={formattedDate}
-                description={parse(initialSentences)}
-                title={card.title}
-                image={card.attachments}
-                isSelected={card.id === selectedCard}
-                handleCardHover={() => handleCardHover(card.id)}
-              />
-            );
-          })}
+          {Array.isArray(cardData) &&
+            cardData.map((card) => {
+              const initialSentences =
+                card.content.split('.').slice(0, 1).join('. ') + '.';
+              const formattedDate = moment(card.date).format(
+                'MMM - Do'
+              );
+              return (
+                <Card
+                  key={card.id}
+                  date={formattedDate}
+                  description={parse(initialSentences)}
+                  title={card.title}
+                  image={card.attachments}
+                  isSelected={card.id === selectedCard}
+                  handleCardHover={() => handleCardHover(card.id)}
+                />
+              );
+            })}
         </motion.div>
       </div>
     </div>
